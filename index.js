4e6955900a772059476bec4ea457504b1794c11b
luis <luiscastro193@gmail.com>
2023-09-26 13:21:54 +0200

    Modernize

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
 "use strict";
-import { rollup } from 'rollup';
+import {rollup} from 'rollup';
 import terser from '@rollup/plugin-terser';
 
 if (process.argv.length < 3)
 	throw "Pass the entry file as an argument";
 
 const filePath = process.argv[2];
-
-rollup({
-	input: filePath,
-	plugins: [terser()]
-}).then(build => {
-	build.write({
-		file: "build.js",
-		format: 'iife'
-	})
-})
+let build = await rollup({input: filePath, plugins: [terser()]});
+await build.write({file: "build.js", format: 'iife'});
